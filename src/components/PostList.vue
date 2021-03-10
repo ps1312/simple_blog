34505c5c8b7bc39b9d0168b12543dc7ce64ef59c
Paulo Sérgio <ps131296@gmail.com>
2021-03-09 21:18:32 -0300

    should display correct number os PostListItem

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,10 +1,17 @@
 <template>
-  <span>No posts around here 🧐</span>
+  <div>
+    <span v-if="posts.length === 0">No posts around here 🧐</span>
+    <post-list-item v-else v-for="post in posts" :key="post.id" :post="post" />
+  </div>
 </template>
 
 <script>
+import PostListItem from "@/components/PostListItem";
 export default {
   name: "PostList",
+  components: {
+    PostListItem
+  },
   props: {
     posts: {
       type: Array,
