4977dffa3fd5fbf52e3f1f64e70de6c3852bafc7
Paulo Sérgio <ps131296@gmail.com>
2021-03-11 14:58:28 -0300

    add hover to navbar menus on lg screens

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,18 +7,18 @@
     </div>
 
     <div class="max-w-4xl mx-auto items-center hidden lg:flex">
-      <router-link to="/"><span class="text-3xl font-bold text-gray-100">pssr.dev</span></router-link>
+      <router-link to="/"><span class="text-3xl font-bold text-gray-100 rounded px-2 hover:bg-indigo-400">pssr.dev</span></router-link>
 
       <div class="inline-flex flex-grow">
         <div class="inline-flex flex-grow">
-            <router-link class="text-lg font-semibold ml-8 text-white font-semibold" to="/">All posts</router-link>
-            <router-link class="text-lg font-semibold ml-8 text-white font-semibold" to="/about">About</router-link>
+            <router-link class="text-lg font-semibold ml-8 text-white font-semibold rounded px-2 hover:bg-indigo-400" to="/">All posts</router-link>
+            <router-link class="text-lg font-semibold ml-8 text-white font-semibold rounded px-2 hover:bg-indigo-400" to="/about">About</router-link>
         </div>
         <div class="inline-flex">
           <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/pssr">
             <base-icon class="w-6 h-6 fill-current text-white rounded hover:bg-indigo-400" icon="linkedin" />
           </a>
-          <a class="pl-4" target="_blank" rel="noopener noreferrer" href="https:www.github.com/ps1312">
+          <a class="ml-4" target="_blank" rel="noopener noreferrer" href="https:www.github.com/ps1312">
             <base-icon class="w-6 h-6 fill-current text-white rounded hover:bg-indigo-400" icon="github" />
           </a>
         </div>
