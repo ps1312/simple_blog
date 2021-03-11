75fb4ccb15f3b14e753e5b2759b59a1c2cac8ee1
Paulo Sérgio <ps131296@gmail.com>
2021-03-11 14:53:08 -0300

    made navbar responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,33 +1,42 @@
 <template>
   <nav class="fixed top-0 w-screen p-4 bg-indigo-500">
-    <div class="flex items-end justify-between text-gray-800">
+    <div class="flex items-end justify-between text-gray-800 lg:hidden">
       <router-link to="/"><span class="text-3xl font-bold text-gray-100">pssr.dev</span></router-link>
 
-      <base-dropdown />
+      <base-dropdown class="lg:hidden" />
+    </div>
+
+    <div class="max-w-4xl mx-auto items-center hidden lg:flex">
+      <router-link to="/"><span class="text-3xl font-bold text-gray-100">pssr.dev</span></router-link>
+
+      <div class="inline-flex flex-grow">
+        <div class="inline-flex flex-grow">
+            <router-link class="text-lg font-semibold ml-8 text-white font-semibold" to="/">All posts</router-link>
+            <router-link class="text-lg font-semibold ml-8 text-white font-semibold" to="/about">About</router-link>
+        </div>
+        <div class="inline-flex">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/pssr">
+            <base-icon class="w-6 h-6 fill-current text-white rounded hover:bg-indigo-400" icon="linkedin" />
+          </a>
+          <a class="pl-4" target="_blank" rel="noopener noreferrer" href="https:www.github.com/ps1312">
+            <base-icon class="w-6 h-6 fill-current text-white rounded hover:bg-indigo-400" icon="github" />
+          </a>
+        </div>
+      </div>
+      
     </div>
   </nav>
 </template>
 
 <script>
 import BaseDropdown from './BaseDropdown';
+import BaseIcon from './BaseIcon';
 
 export default {
-  components: { BaseDropdown },
+  components: {
+    BaseDropdown,
+    BaseIcon,
+  },
   name: "NavBar"
 }
-// <div class="max-w-4xl mx-auto flex justify-between">
-//       <div class="inline-flex  items-end">
-//           <router-link to="/"><span class="text-4xl font-bold">pssr.dev</span></router-link>
-//           <router-link class="text-lg font-semibold ml-8" to="/">All posts</router-link>
-//           <router-link class="text-lg font-semibold ml-8" to="/about">About</router-link>
-//       </div>
-//       <div class="flex items-center">
-//         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/pssr">
-//           <img class="w-8 h-8 mr-8" src="../assets/linkedin.svg" />
-//         </a>
-//         <a target="_blank" rel="noopener noreferrer" href="https:www.github.com/ps1312">
-//           <img class="w-8 h-8" src="../assets/github.svg" />
-//         </a>
-//       </div>
-//     </div>
 </script>
