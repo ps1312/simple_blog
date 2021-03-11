5fb63a9a791e832a0a8c08b8ee05d19683143c3c
Paulo Sérgio <ps131296@gmail.com>
2021-03-10 23:27:00 -0300

    add dropdown to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="w-screen p-4 border-b  border-gray-300 fixed top-0 bg-white">
     <div class="max-w-4xl mx-auto flex justify-between">
-      <div>
+      <div class="inline-flex">
           <router-link to="/"><span class="text-4xl font-bold">pssr.dev</span></router-link>
           <router-link class="text-lg font-semibold ml-8" to="/">All posts</router-link>
           <router-link class="text-lg font-semibold ml-8" to="/about">About</router-link>
