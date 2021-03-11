4431ab23912413ee8c918b1b6c0444e67346736d
Paulo Sérgio <ps131296@gmail.com>
2021-03-10 23:12:10 -0300

    add navbar with index and about

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app" class="antialiase">
-    <main class="container mx-auto px-8 md:max-w-3xl">
+    <header>
+      <nav-bar></nav-bar>
+    </header>
+    <main class="container mx-auto px-8 md:max-w-3xl mt-24">
       <router-view />
     </main>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
 export default {
   name: "App",
+  components: {
+    NavBar,
+  }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="w-screen p-4 border-b  border-gray-300 fixed top-0 bg-white">
+    <div class="max-w-4xl mx-auto">
+      <router-link to="/"><span class="text-4xl font-bold">pssr.dev</span></router-link>
+
+      <router-link class="text-lg font-semibold ml-8" to="/">All posts</router-link>
+      <router-link class="text-lg font-semibold ml-8" to="/about">About</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
